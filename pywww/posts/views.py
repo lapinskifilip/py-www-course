79015cea0aa7b3d03b32362dc08bfeaa914fe2e9
Filip Łapiński <lapinski.filipp@gmail.com>
2022-10-12 00:17:14 +0200

    some update

--- pywww/posts/views.py
+++ pywww/posts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 
+
+def post_details(request):
+    post = Post.objects.first()
+    context = {'post_details': post}
+
+    return render(request, "posts/details.html", context)
 def posts_list(request):
     posts = Post.objects.all()
     context = {'posts_list': posts}
