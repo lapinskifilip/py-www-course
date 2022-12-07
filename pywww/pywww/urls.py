3e858ec574e2d345544f086aa5502186dede7ad4
Filip Łapiński <lapinski.filipp@gmail.com>
2022-12-07 20:17:52 +0100

    upload/download file

--- pywww/pywww/urls.py
+++ pywww/pywww/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('', include("main.urls")),
     path('posts/', include("posts.urls")),
     path('books/', include("books.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
